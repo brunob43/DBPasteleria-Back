afb0c382a920f82a0970f0323c3f9be05ee7c182
LarryJose <larryjoseguarenas@gmail.com>
2023-04-24 17:34:38 -0300

    actualizacion

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { connect } from "mongoose";
 import Product from "./models/producto";
 
 async function connectDB() {
-  const db = await connect('mongodb://localhost:27017/Productos')
+  const db = await connect("mongodb://0.0.0.0:27017/Productos")
   console.log('server is connected to' + " " + db.connection.db.databaseName)
 }
 
 connectDB()
 
 async function saveData() {
-  
+
   const cake = new Product({
   name: "Cake",
   description:"Chocolate Cake with Chips",
@@ -26,3 +26,4 @@ async function saveData() {
 }
 
 saveData()
+
